25276043aa754c107770fa640284b3e0c8dea2aa
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-04-29 22:09:40 +0900

    react components part 1

---
+++ components/MenuComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Flatlist } from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { FlatList } from 'react-native-gesture-handler';
+
+function Menu(props) {
+
+    const renderMenuItem = ({item, index}) => {
+        return(
+            <ListItem
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                leftAvatar={{ source: require('./images/uthappizza.png')}}
+                />
+        );
+    }
+
+    return(
+        <FlatList
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyExtractor={item => item.id.toString()}
+            />
+    );
+}
+
+export default Menu;
