c311a604e104c77ebdb4d0fef3eb1a495e4ad0ae
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-04-30 23:03:13 +0900

    React native navigation part 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Card } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
+
 
 function RenderDish(props) {
     const dish = props.dish;
@@ -22,10 +24,28 @@ function RenderDish(props) {
     }
 }
 
-function Dishdetail(props) {
-    return(
-        <RenderDish dish={props.dish} /> 
-    );
+// class component
+class Dishdetail extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    static navigationOptions = {
+        title: 'Dish Details'
+    };
+
+    render() {
+        const dishId = this.props.navigation.getParam('dishId', '')
+
+        return(
+            <RenderDish dish={this.state.dishes[+dishId]} />
+        );
+    }
+    
 }
 
 export default Dishdetail;
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -1,30 +1,49 @@
 import React, { Component } from 'react';
 import { View, Flatlist } from 'react-native';
 import { ListItem } from 'react-native-elements';
+import { DISHES } from '../shared/dishes';
 import { FlatList } from 'react-native-gesture-handler';
 
-function Menu(props) {
 
-    const renderMenuItem = ({item, index}) => {
+class Menu extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
+
+    // local navOptions
+    static navigationOptions = {
+        title: 'Menu'
+    };
+
+    render() {
+        const renderMenuItem = ({item, index}) => {
+            return(
+                <ListItem
+                    key={index}
+                    title={item.name}
+                    subtitle={item.description}
+                    hideChevron={true}
+                    onPress={() => navigate('Dishdetail', { dishId: item.id })}
+                    leftAvatar={{ source: require('./images/uthappizza.png') }}
+                    />
+            );
+        }
+
+        const { navigate } = this.props.navigation;
+
         return(
-            <ListItem
-                key={index}
-                title={item.name}
-                subtitle={item.description}
-                hideChevron={true}
-                onPress={() => props.onPress(item.id)}
-                leftAvatar={{ source: require('./images/uthappizza.png')}}
+            <FlatList
+                data={this.state.dishes}
+                renderItem={renderMenuItem}
+                keyExtractor={item => item.id.toString()}
                 />
         );
     }
-
-    return(
-        <FlatList
-            data={props.dishes}
-            renderItem={renderMenuItem}
-            keyExtractor={item => item.id.toString()}
-            />
-    );
+    
 }
 
 export default Menu;
