50c7790f941e2bd0c53c271876b4eff19955f87d
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-05-02 17:09:52 +0900

    Buttons and icons

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { LEADERS } from '../shared/leaders';
 import { FlatList } from 'react-native-gesture-handler';
@@ -10,6 +10,28 @@ function History() {
         <Card title="Our History">
             <Text>Started in 2010, Ristorante con Fusion quickly established itself as a culinary icon par excellence in Hong Kong. With its unique brand of world fusion cuisine that can be found nowhere else, it enjoys patronage from the A-list clientele in Hong Kong.  Featuring four of the best three-star Michelin chefs in the world, you never know what will arrive on your plate the next time you visit us.</Text>
             <Text>The restaurant traces its humble beginnings to The Frying Pan, a successful chain started by our CEO, Mr. Peter Pan, that featured for the first time the world's best cuisines in a pan.</Text>
+        </Card>
+    )
+}
+
+function RenderLeaders(props) {
+    const leaders = props.leaders;
+
+    const renderLeaderItem = ({item, index}) => {
+        return(
+            <ListItem key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                leftAvatar={{ source: require('./images/alberto.png')}} />
+        );
+    }
+
+    return(
+        <Card title="Corporate Leadership">
+            <FlatList data={leaders}
+                renderItem={renderLeaderItem}
+                keyExtractor={leader => leader.id.toString() } />
         </Card>
     )
 }
@@ -27,27 +49,11 @@ class About extends Component {
     };
 
     render() {
-        const renderLeaderItem = ({item, index}) => {
-            return(
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    hideChevron={true}
-                    leftAvatar={{ source: require('./images/alberto.png')}} />
-            );
-        }
-
         return(
-            <View> 
+            <ScrollView> 
                 <History />
-                <Card title="Corporate Leadership">
-                    <FlatList
-                        data={this.state.leaders}
-                        renderItem={renderLeaderItem}
-                        keyExtractor={leader => leader.id.toString() } />
-                </Card>
-            </View>    
+                <RenderLeaders leaders={this.state.leaders} />
+            </ScrollView>    
         );
     }
 }
