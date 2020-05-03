6fc7457966cdebfa48103d52fbff3cd232fd586d
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-05-03 18:20:47 +0900

    Persist redux store

--- App.js
+++ App.js
@@ -2,15 +2,28 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
+// import { PersistGate } from 'redux-persist/es/integration';
+import { PersistGate } from 'redux-persist/integration/react'
+import { Loading } from './components/LoadingComponent';
+
+import { AsyncStorage } from 'react-native';
+ const config = {
+        key: 'root',
+        debug: true,
+        storage: AsyncStorage
+    };
 
 
-const store = ConfigureStore();
+const {persistor, store} = ConfigureStore();
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Main />
+        <PersistGate loading={<Loading />}
+          persistor={persistor} >
+          <Main />
+        </PersistGate>
       </Provider>
     );
   }
